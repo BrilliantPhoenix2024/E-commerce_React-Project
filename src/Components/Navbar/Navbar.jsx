915b51560022daf43489ec11c6c4714bb1224ccc
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2024-07-30 15:16:37 +0330

    Fix the bugs in functions

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,68 +7,38 @@ import { ShopContext } from "../../Context/ShopContext";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
-  const { getTotalCartItems } = useContext(ShopContext);
+  const { getTotalCartItems } = useContext(ShopContext); // Get the context
 
   return (
     <div className="navbar">
       <Link to="/">
-        {" "}
         <div className="nav-logo">
-          {" "}
           <img src={logo} alt="" />
           <p>SHOPPER</p>
-        </div>{" "}
+        </div>
       </Link>
 
       <ul className="nav-menu">
-        <li
-          className="nav-menu"
-          onClick={() => {
-            setMenu("shop");
-          }}
-        >
-          {" "}
-          <Link to="/"> SHOP {menu === "shop" ? <hr /> : <></>} </Link>
+        <li className="nav-menu" onClick={() => setMenu("shop")}>
+          <Link to="/">SHOP {menu === "shop" ? <hr /> : null}</Link>
         </li>
-
-        <li
-          className="nav-menu"
-          onClick={() => {
-            setMenu("mens");
-          }}
-        >
-          <Link to="/mens"> MEN {menu === "mens" ? <hr /> : <></>} </Link>
+        <li className="nav-menu" onClick={() => setMenu("mens")}>
+          <Link to="/mens">MEN {menu === "mens" ? <hr /> : null}</Link>
         </li>
-
-        <li
-          className="nav-menu"
-          onClick={() => {
-            setMenu("womens");
-          }}
-        >
-          {" "}
-          <Link to="/womens"> WOMEN {menu === "womens" ? <hr /> : <></>}</Link>
+        <li className="nav-menu" onClick={() => setMenu("womens")}>
+          <Link to="/womens">WOMEN {menu === "womens" ? <hr /> : null}</Link>
         </li>
-
-        <li
-          className="nav-menu"
-          onClick={() => {
-            setMenu("kids");
-          }}
-        >
-          {" "}
-          <Link to="/kids"> KIDS {menu === "kids" ? <hr /> : <></>} </Link>
+        <li className="nav-menu" onClick={() => setMenu("kids")}>
+          <Link to="/kids">KIDS {menu === "kids" ? <hr /> : null}</Link>
         </li>
       </ul>
 
       <div className="nav-login-card">
         <Link to="/login">
-          {" "}
           <button>Login</button>
         </Link>
-
         <Link to="/cart">
-          <img src={cart_icon} alt="" />
+          <img src={cart_icon} alt="Cart" />
         </Link>
         <div className="nav-cart-count">{getTotalCartItems()}</div>
       </div>
